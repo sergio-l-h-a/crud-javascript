e620b431a1dcdcd0c21304061cbb9de6a495caaf
Francisco-Sergio <lanhousearte@outlook.com>
2026-03-10 13:47:03 -0300

    Finalizando a primeira versão do projeto

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,10 +1,13 @@
-const mysql = require('mysql2/promise');
+require("dotenv").config();
+const mysql = require("mysql2/promise");
+
 
 const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '55q2w3e4A!',
-    database: 'cadastro_usuario'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD, 
+    database: process.env.DB_DATABASE
 });
 
 module.exports = db;
+
